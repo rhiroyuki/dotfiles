6669d308eca7d067a40469d42104dfa9419fd0ce
Ricardo Eihara <ricardo.hejr@gmail.com>
2021-09-03 00:00:47 -0300

    Move zshrc file to zshrc_dotfile
    A lot of scripts end up writing in .zshrc. This commit changes the file to be added to source on zshrc preventing from having to unnecessary change the file just to commit to the repo.

--- install.sh
+++ install.sh
@@ -2,23 +2,47 @@
 
 set -eu pipefail
 
+backup_existing_file () {
+  source_file="$1"
+
+  if [ -e "$source_file" ]; then
+    mv "$source_file" "${source_file}_backup_$(date +%s%3N)"
+  fi
+}
+
+ln_file_to_home_directory () {
+  source_full_path="$HOME/dotfiles/$1"
+  target_full_path=${2:-"$HOME/.$1"} # default arg
+
+  backup_existing_file "$target_full_path"
+
+  ln -s "$source_full_path" "$target_full_path"
+}
+
 install_nvim () {
   source_full_path="$HOME/dotfiles/nvim/*"
   target_full_path="$HOME/.config/nvim/"
 
-  # checking if folder already exists
-  if [ -e "$HOME/.config/nvim" ]; then
-    mv "$HOME/.config/nvim" "$HOME/.config/nvim_backup_$(date +%s%3N)"
-  fi
+  backup_existing_file "$HOME/.config/nvim"
 
   mkdir -p "$HOME/.config/nvim"
 
-  # symbolic linking all files
   ln -s "$HOME/dotfiles/nvim/"* "$HOME/.config/nvim/"
 }
 
+install_zshrc() {
+  ln_file_to_home_directory 'zshrc_dotfile'
+
+  [ ! -f "$HOME/.zshrc" ] && touch "$HOME/.zshrc"
+
+  # shellcheck disable=SC2016
+  literal_text='source $HOME/.zshrc_dotfile'
+
+  grep -qxF "$literal_text" "$HOME/.zshrc" || echo -e "${literal_text}\n$(cat "$HOME/.zshrc")" > "$HOME/.zshrc"
+}
+
 install_dotfiles () {
-  dotfiles=( asdfrc default-gems gemrc tmux.conf zshrc aliases gitignore solargraph.yml reek.yml gitattributes )
+  dotfiles=( asdfrc default-gems gemrc tmux.conf aliases gitignore solargraph.yml reek.yml gitattributes )
 
   for dotfile in "${dotfiles[@]}";
   do
@@ -26,21 +50,9 @@ install_dotfiles () {
   done
 }
 
-ln_file_to_home_directory () {
-  source_full_path="$HOME/dotfiles/$1"
-  target_full_path=${2:-"$HOME/.$1"}
-
-  # checking if file exists to move it if necessary
-  if [ -e "$target_full_path" ]; then
-    mv "$target_full_path" "${target_full_path}_backup_$(date +%s)"
-  fi
-
-  # symbolic linking file
-  ln -s "$source_full_path" "$target_full_path"
-}
-
 main () {
   git clone https://github.com/rhiroyuki/dotfiles.git "$HOME/dotfiles"
+  install_zshrc
   install_dotfiles
   install_nvim
 
