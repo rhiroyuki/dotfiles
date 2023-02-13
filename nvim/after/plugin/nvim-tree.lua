3417fa6bf75f4155c70f1fd9907d9d8549486cb5
Ricardo Eihara <ricardo.hejr@gmail.com>
2023-02-12 22:34:38 -0300

    Open nvim-tree when target is a directory

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -30,5 +30,23 @@ if packer_plugins["nvim-tree.lua"] and packer_plugins["nvim-tree.lua"].loaded th
       },
     })
 
+  local function open_nvim_tree(data)
+
+    -- buffer is a directory
+    local directory = vim.fn.isdirectory(data.file) == 1
+
+    if not directory then
+      return
+    end
+
+    -- change to the directory
+    vim.cmd.cd(data.file)
+
+    -- open the tree
+    require("nvim-tree.api").tree.open()
+  end
+
+  vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
   vim.keymap.set('n', '<C-\\>', ':NvimTreeFindFileToggle<cr>', { noremap = true, silent = true })
 end
