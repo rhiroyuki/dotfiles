086191a78eb011808436a900c36f96ad0608d765
Ricardo Eihara <ricardo.hejr@gmail.com>
2023-10-06 17:52:47 -0300

    Add keymap to telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -19,6 +19,7 @@ return {
     local builtin = require('telescope.builtin')
 
     vim.keymap.set('n', '<C-p>', builtin.find_files, { noremap = true })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { noremap = true })
     vim.keymap.set('n', '<leader>h', builtin.oldfiles, { noremap = true })
   end
 }
