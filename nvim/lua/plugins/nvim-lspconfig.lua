9aef7421557deb18744eaaa6e4e061bc76a88915
Ricardo Eihara <ricardo.hejr@gmail.com>
2023-10-08 17:43:36 -0300

    Detect which ruby lsp to start

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -10,10 +10,35 @@ return {
     },
     config = function()
       local lspconfig = require('lspconfig')
-
       local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local group = vim.api.nvim_create_augroup("DetectWhichLspRubyToStart", { clear = true })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "ruby",
+        once = true,
+        group = group,
+        callback = function()
+          vim.schedule(function()
+            local root_path = vim.fs.dirname(vim.fs.find({ 'Gemfile' }, { upward = true })[1])
+            local grep_command = "grep -E -i \"gem.*standard[\\\"'].*\""
+            local grep_result = vim.fn.system(table.concat({ grep_command, " ", root_path, "/Gemfile" }))
+            if grep_result == nil or grep_result == '' then
+              vim.cmd("LspStart solargraph")
+            else
+              vim.cmd("LspStart standardrb")
+            end
+          end)
+        end
+      })
 
       local default_setup = function(server)
+        lspconfig[server].setup({
+          autostart = true,
+          capabilities = lsp_capabilities
+        })
+      end
+
+      local stopped_setup = function(server)
         lspconfig[server].setup({
           autostart = false,
           capabilities = lsp_capabilities
@@ -25,6 +50,12 @@ return {
         ensure_installed = {},
         handlers = {
           default_setup,
+          standardrb = function()
+            stopped_setup("standardrb")
+          end,
+          solargraph = function()
+            stopped_setup("solargraph")
+          end,
           lua_ls = function()
             lspconfig.lua_ls.setup({
               settings = {
