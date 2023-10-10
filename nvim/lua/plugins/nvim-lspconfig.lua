627c260b6021bcf5e6a5a4cce915e77137c56d47
Ricardo <ricardo.hejr@gmail.com>
2023-10-10 00:34:11 -0300

    Handle diagnostics for multiple ruby lsps running

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -13,6 +13,12 @@ return {
       local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
       local group = vim.api.nvim_create_augroup("DetectWhichLspRubyToStart", { clear = true })
 
+      local default_opt = { autostart = true, capabilities = lsp_capabilities }
+
+      local lsp_server_setup = function(server, opt)
+        lspconfig[server].setup(vim.tbl_deep_extend("force", default_opt, opt or {}))
+      end
+
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "ruby",
         once = true,
@@ -23,39 +29,26 @@ return {
             local grep_command = "grep -E -i \"gem.*standard[\\\"'].*\""
             local grep_result = vim.fn.system(table.concat({ grep_command, " ", root_path, "/Gemfile" }))
             if grep_result == nil or grep_result == '' then
+              lsp_server_setup("solargraph")
               vim.cmd("LspStart solargraph")
             else
+              lsp_server_setup("solargraph", { handlers = { ['textDocument/publishDiagnostics'] = function() end } })
+              lsp_server_setup("standardrb")
+
+              vim.cmd("LspStart solargraph")
               vim.cmd("LspStart standardrb")
             end
           end)
         end
       })
 
-      local default_setup = function(server)
-        lspconfig[server].setup({
-          autostart = true,
-          capabilities = lsp_capabilities
-        })
-      end
-
-      local stopped_setup = function(server)
-        lspconfig[server].setup({
-          autostart = false,
-          capabilities = lsp_capabilities
-        })
-      end
-
       require('mason').setup({ autostart = false })
       require('mason-lspconfig').setup({
         ensure_installed = {},
         handlers = {
-          default_setup,
-          standardrb = function()
-            stopped_setup("standardrb")
-          end,
-          solargraph = function()
-            stopped_setup("solargraph")
-          end,
+          lsp_server_setup,
+          standardrb = function() end,
+          solargraph = function() end,
           lua_ls = function()
             lspconfig.lua_ls.setup({
               settings = {
