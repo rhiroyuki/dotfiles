909ea61b5f321385cfe56eaf0e3238e93deec575
Ricardo <ricardo.hejr@gmail.com>
2023-10-10 00:34:48 -0300

    Add supertab

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -5,19 +5,48 @@ return {
     event = 'InsertEnter',
     dependencies = {
       { 'L3MON4D3/LuaSnip' },
+      { 'saadparwaiz1/cmp_luasnip' }
     },
     config = function()
       local cmp = require('cmp')
+      local luasnip = require('luasnip')
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
 
       local select_opts = { behavior = cmp.SelectBehavior.Select }
 
       local cmp_config = {
         snippet = {
           expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
           end,
         },
         mapping = {
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
           ['<CR>'] = cmp.mapping.confirm({ select = false }),
           ['<C-p>'] = cmp.mapping(function()
             if cmp.visible() then
@@ -58,8 +87,14 @@ return {
           { name = 'copilot',                keyword_length = 0 },
           { name = 'nvim_lsp_signature_help' },
           { name = 'nvim_lsp' },
+          { name = 'luasnip' },
         }
       }
+
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
 
       cmp.setup(cmp_config)
     end
