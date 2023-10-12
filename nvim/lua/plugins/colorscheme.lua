a824c1da593a1f88f873cf576794968ba7833bd3
Ricardo <ricardo.hejr@gmail.com>
2023-10-12 17:56:43 -0300

    Change colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,58 @@
 return {
   {
-    'ajmwagar/vim-deus',
-    lazy = false,
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     config = function()
-      vim.opt.background = 'dark'
-      vim.cmd.colorscheme('deus')
+      require("catppuccin").setup({
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = false,
+          telescope = {
+            enabled = true
+          },
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+            },
+            underlines = {
+              errors = { "underline" },
+              hints = { "underline" },
+              warnings = { "underline" },
+              information = { "underline" },
+            },
+            inlay_hints = {
+              background = true,
+            },
+          },
+          indent_blankline = {
+            enabled = true,
+            scope_color = "", -- catppuccin color (eg. `lavender`) Default: text
+            colored_indent_levels = false,
+          },
+        }
+      })
+
+      vim.cmd.colorscheme('catppuccin-macchiato')
     end
-  },
+  }
+  -- {
+  --   'ajmwagar/vim-deus',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.opt.background = 'dark'
+  --     vim.cmd.colorscheme('deus')
+  --   end
+  -- },
   -- 'ajmwagar/lightline-deus',
   -- 'itchyny/lightline.vim'
 }
