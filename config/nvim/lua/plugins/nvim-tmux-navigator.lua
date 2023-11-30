549d1af46b2fe7b125ecd67522dd4e58a5ed2034
Ricardo Eihara <ricardo.hejr@gmail.com>
2023-11-30 16:49:23 -0300

    Update vim-tmux-navigator to nvim-tmux-navigator

---
+++ config/nvim/lua/plugins/nvim-tmux-navigator.lua
@@ -0,0 +1,15 @@
+return {
+  "alexghergh/nvim-tmux-navigation",
+  event = "VeryLazy",
+  config = function()
+    require("nvim-tmux-navigation").setup({
+      disable_when_zoomed = true,
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>"
+      }
+    })
+  end
+}
