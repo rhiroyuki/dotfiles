6b5c8f49377fd117ddf38cd5d40326677ed43055
Ricardo Eihara <ricardo.hejr@gmail.com>
2025-01-23 21:40:38 -0300

    Dynamically handle node path

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -7,7 +7,20 @@ return {
       require("copilot").setup({
         suggestion = { enabled = false, auto_trigger = false },
         panel = { enabled = false },
-        copilot_node_command = '/bin/node' -- Avoid having to install nodejs in every project
+        copilot_node_command = (function()
+          -- Dynamically determine the path to the node executable
+          -- And avoid to use node from current project directory that vim is opened
+          local paths = {"/bin/node", "/usr/bin/node"}
+          for _, path in ipairs(paths) do
+            if os.execute("test -x " .. path) then
+              return path
+            end
+          end
+          local handle = io.popen("which node")
+          local result = handle:read("*a")
+          handle:close()
+          return result:gsub("%s+", "") -- Remove any trailing whitespace
+        end)()
       })
     end,
   },
