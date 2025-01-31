6c31028f2195753c191fd9a5c90baae60cf57573
Ricardo Eihara <ricardo.hejr@gmail.com>
2025-01-30 21:34:39 -0300

    Add ollama as provider to avante nvim

--- config/nvim/lua/plugins/avante.lua
+++ config/nvim/lua/plugins/avante.lua
@@ -4,8 +4,53 @@ return {
   lazy = false,
   version = false, -- set this if you want to always pull the latest change
   opts = {
-    -- add any opts here
     provider = "copilot"
+    -- provider = "ollama",
+    -- vendors = {
+    -- ---@type AvanteProvider
+    --   ollama = {
+    --     api_key_name = "",
+    --     ask = "",
+    --     endpoint = "http://127.0.0.1:11434/api",
+    --     model = "deepseek-r1:14b",
+    --     -- model = "phi4",
+    --     -- model = "qwen2.5-coder:14b",
+    --     -- model = "llama3.1:latest",
+    --     parse_curl_args = function(opts, code_opts)
+    --       return {
+    --         url = opts.endpoint .. "/chat",
+    --         headers = {
+    --           ["Accept"] = "application/json",
+    --           ["Content-Type"] = "application/json",
+    --         },
+    --         body = {
+    --           model = opts.model,
+    --           options = {
+    --             num_ctx = 16384,
+    --           },
+    --           messages = require("avante.providers").copilot.parse_messages(code_opts), -- you can make your own message, but this is very advanced
+    --           stream = true,
+    --         },
+    --       }
+    --     end,
+    --     parse_stream_data = function(data, handler_opts)
+    --       -- Parse the JSON data
+    --       local json_data = vim.fn.json_decode(data)
+    --       -- Check for stream completion marker first
+    --       if json_data and json_data.done then
+    --         handler_opts.on_complete(nil)  -- Properly terminate the stream
+    --         return
+    --       end
+    --       -- Process normal message content
+    --       if json_data and json_data.message and json_data.message.content then
+    --         -- Extract the content from the message
+    --         local content = json_data.message.content
+    --         -- Call the handler with the content
+    --         handler_opts.on_chunk(content)
+    --       end
+    --     end,
+    --   },
+    -- },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
@@ -15,7 +60,7 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    -- "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
