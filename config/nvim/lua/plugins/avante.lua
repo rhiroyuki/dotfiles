bd2d3c637a82636ff079504579b5425cbe9f3e0d
Ricardo Eihara <ricardo.hejr@gmail.com>
2025-04-17 08:34:23 -0300

    Testing avante using ollama

--- config/nvim/lua/plugins/avante.lua
+++ config/nvim/lua/plugins/avante.lua
@@ -4,53 +4,25 @@ return {
   lazy = false,
   version = false, -- set this if you want to always pull the latest change
   opts = {
-    provider = "copilot"
+    provider = "copilot",
     -- provider = "ollama",
     -- vendors = {
-    -- ---@type AvanteProvider
+    -- -- ---@type AvanteProvider
     --   ollama = {
+    --     __inherited_from = "openai",
     --     api_key_name = "",
-    --     ask = "",
-    --     endpoint = "http://127.0.0.1:11434/api",
-    --     -- model = "deepseek-r1:14b",
-    --     model = 'deepseek-r1:8b',
-    --     -- model = "phi4",
-    --     -- model = "qwen2.5-coder:14b",
-    --     -- model = "llama3.1:latest",
-    --     parse_curl_args = function(opts, code_opts)
-    --       return {
-    --         url = opts.endpoint .. "/chat",
-    --         headers = {
-    --           ["Accept"] = "application/json",
-    --           ["Content-Type"] = "application/json",
-    --         },
-    --         body = {
-    --           model = opts.model,
-    --           options = {
-    --             num_ctx = 16384,
-    --           },
-    --           messages = require("avante.providers").copilot.parse_messages(code_opts), -- you can make your own message, but this is very advanced
-    --           stream = true,
-    --         },
-    --       }
-    --     end,
-    --     parse_stream_data = function(data, handler_opts)
-    --       -- Parse the JSON data
-    --       local json_data = vim.fn.json_decode(data)
-    --       -- Check for stream completion marker first
-    --       if json_data and json_data.done then
-    --         handler_opts.on_complete(nil)  -- Properly terminate the stream
-    --         return
-    --       end
-    --       -- Process normal message content
-    --       if json_data and json_data.message and json_data.message.content then
-    --         -- Extract the content from the message
-    --         local content = json_data.message.content
-    --         -- Call the handler with the content
-    --         handler_opts.on_chunk(content)
-    --       end
-    --     end,
+    --     endpoint = "http://127.0.0.1:11434/v1",
+    --     model = "qwen2.5-coder:14b",
+    --     -- model = "qwen2.5-coder:32b",
+    --     disable_tools = true, -- Open-source models often do not support tools.
     --   },
+    --   -- ollama = {
+    --   --   endpoint = "http://0.0.0.0:11434/api",
+    --   --   -- model = "deepseek-r1:14b",
+    --   --   -- model = 'deepseek-r1:8b',
+    --   --   -- model = "phi4",
+    --   --   -- model = "llama3.1:latest",
+    --   -- },
     -- },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
