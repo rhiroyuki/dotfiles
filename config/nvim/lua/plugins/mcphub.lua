7b8803cfb7e43a7edabe3eeedbfce1f25a47c377
Ricardo Eihara <ricardo.hejr@gmail.com>
2025-09-08 08:57:46 -0300

    WIP

--- config/nvim/lua/plugins/mcphub.lua
+++ config/nvim/lua/plugins/mcphub.lua
@@ -8,6 +8,13 @@ return {
     config = function()
       require('mcphub').setup({
         use_bundled_binary = true,
+        extensions = {
+            codecompanion = {
+                show_result_in_chat = true,
+                make_vars = true,
+                make_slash_commands = true,
+            }
+        }
       })
     end,
   }
